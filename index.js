c436a7e23207503c42bf9b82f5ef19108eaf53bf
Lugertr <86018433+Lugertr@users.noreply.github.com>
2021-11-06 06:19:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
+import Coder from "./src/ceaser.js";
 import Generate from "./src/Generate.js";
 import helman from "./src/Diffi/helman.js";
 import DoRSA from "./src/RSA/DoRSA.js";
-import HashClient from "./src/SPR/SPRClient.js";
 
 //const TestNumber = new helman();
 
@@ -10,10 +9,10 @@ const code = "src/txt/test.txt";
 const decode = "src/txt/Decod.txt"
 const over = "src/txt/analitik.txt"
 
-//const test = new Coder(code,decode,2);
-//test.analiz(decode,over)
-//test.analizbigram(decode,over)
-//est.print();
+const test = new Coder(code,decode,2);
+test.analiz(decode,over)
+test.analizbigram(decode,over)
+//test.print();
 
 //for (var i=0; i<1000;i++) {
 //const TestNumber = new helman();
@@ -27,9 +26,4 @@ const over = "src/txt/analitik.txt"
 //  }
 
 //const  TestRSA = new DoRSA();
-const  HashF = new HashClient(777,2);
-
-
-//const DateB = new Database();
-//DateB.add();
-
+//const  HashF = new HashClient(777,2);
